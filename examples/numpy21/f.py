7833f6603d6fc86c60e9813d9cef4198b879c4c0
JaimeZepeda08 <jzepedapina@gmail.com>
2026-03-05 09:12:47 -0600

    added version prints

--- examples/numpy21/f.py
+++ examples/numpy21/f.py
@@ -2,6 +2,8 @@ import numpy
 
 def f(event):
     return {
+        "pinned_version": "1.23.5",
+        "actual_version": numpy.__version__,
         'result': int(numpy.array(event).sum()),
         'numpy-version': numpy.__version__
     }
